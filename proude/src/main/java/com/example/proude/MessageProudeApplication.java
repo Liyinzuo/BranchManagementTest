af97ed63d09d53228e25174a7bd444c00a767c1e
Emp <81659973@163.com>
2021-02-19 09:49:06 +0800

    1

---
+++ proude/src/main/java/com/example/proude/MessageProudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.proude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageProudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageProudeApplication.class, args);
+    }
+
+}
